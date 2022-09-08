130d8f3aca206dffb730fad499fb6a9528a937ca
Maurizio Tolomeo (Moris) <37340833+moris88@users.noreply.github.com>
2022-09-08 15:18:28 +0200

    :tada: init commit

---
+++ client/src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const App = () => {
+    return (
+        <div>
+            <p>
+                test 2.0
+            </p>
+        </div>
+    )
+}
+
+export default App
---
+++ server/src/server.tsx
@@ -0,0 +1,42 @@
+import express from 'express'
+import * as ReactDOMServer from 'react-dom/server'
+import React from 'react'
+import App from '../../client/src/App'
+import dotenv from 'dotenv'
+
+const { log, error } = console
+
+const ENV = dotenv.config().parsed
+const app = express()
+const port = ENV.PORT || 3000
+const title = ENV.TITLE || 'SSR React'
+
+try {
+    app.get('/', (req, res) => {
+        const server = ReactDOMServer.renderToString(<App />)
+        const html = `
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8" />
+        <link rel="icon" type="image/svg+xml" href="/vite.svg" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <title>${title}</title>
+        <script src="app.js" async defer></script>
+    </head>
+    <body>
+        <div id="root">${server}</div>
+    </body>
+</html>
+        `
+        res.send(html)
+    })
+
+    app.use(express.static('./build'))
+
+    app.listen(port, () => {
+        log(`Server listening on port: ${port}`)
+    })
+} catch(err) {
+    error(err)
+}
